4f48ad13b4cc7540ea93d23a2b7554893260162f
Overcommon7 <natecapitalization@gmail.com>
2022-11-26 17:55:35 -0800

    Finished Implementing Inventory

--- Assignment5/Assignment5/Program.cs
+++ Assignment5/Assignment5/Program.cs
@@ -11,6 +11,13 @@ namespace Assignment5
             // TODO: Create an inventory
             // TODO: Add 2 items to the inventory
             // Verify the number of items in the inventory.
+
+            Inventory inventory = new Inventory(5);
+            var axe = new Item("Axe", 1, ItemGroup.Equipment);
+            var potion = new Item("Health Potion", 3, ItemGroup.Consumable);
+            inventory.AddItem(potion);
+            inventory.AddItem(axe);
+            Console.WriteLine(inventory.AvailableSlots);
         }
     }
 }
